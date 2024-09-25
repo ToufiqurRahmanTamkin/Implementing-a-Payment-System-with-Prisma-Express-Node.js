538d503a143636a12c754c90783a08b5f96027cd
Toufiqur Rahman Tamkin <41835890+ToufiqurRahmanTamkin@users.noreply.github.com>
2024-09-26 01:06:54 +0600

    Create auth middleware for validating user

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const prisma = require('../config/database');
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await prisma.user.findUnique({ where: { id: decoded.userId } });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'Please authenticate.' });
+  }
+};
