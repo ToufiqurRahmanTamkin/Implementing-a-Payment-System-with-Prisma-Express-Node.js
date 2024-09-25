782bf6875f483c2ba1549bd3ead3109e896cf576
Toufiqur Rahman Tamkin <41835890+ToufiqurRahmanTamkin@users.noreply.github.com>
2024-09-25 16:02:11 +0600

    Create auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,32 @@
+const { validationResult } = require('express-validator');
+const authService = require('../services/authService');
+
+exports.register = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    const result = await authService.register(email, password);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    const result = await authService.login(email, password);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
