8e35aa4f2137bdc7b5b6cd5533773999ca8c5d47
ToufiqurRahmanTamkin <tamkin22rahman@gmail.com>
2024-09-29 07:31:01 +0600

    user data validation added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,6 +11,12 @@ exports.register = async (req, res, next) => {
     }
 
     const { email, password } = req.body;
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already registered" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await prisma.user.create({
       data: { email, password: hashedPassword },
